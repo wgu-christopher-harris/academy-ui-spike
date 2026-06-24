dbdb529161b1f161122d5c2199b36438bbc5a841
chris harris <chris.harris1@wgu.edu>
2026-06-24 17:09:26 -0500

    Simplify enrollment app component by removing unused services, effects, and template logic

--- apps/enrollment/src/app/app.ts
+++ apps/enrollment/src/app/app.ts
@@ -1,27 +1,15 @@
-import {Component, effect, inject} from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
-import {EnrollmentCatalogService} from "@academy/enrollment/data-access";
 import {ButtonModule} from "primeng/button";
-import {JsonPipe} from "@angular/common";
 import {CardModule} from "primeng/card";
 import {ProgressSpinnerModule} from "primeng/progressspinner";
 
 @Component({
-  imports: [NxWelcome, RouterModule, ButtonModule, JsonPipe, CardModule, ProgressSpinnerModule],
+  imports: [RouterModule, ButtonModule, CardModule, ProgressSpinnerModule],
   selector: 'acad-enroll-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected title = 'enrollment';
-  catalogService = inject(EnrollmentCatalogService);
-  catalogResource = this.catalogService.catalogCourses;
-
-  constructor() {
-    effect(() => {
-      console.log(this.catalogResource.value());
-    });
-    this.catalogService.getEnrollmentCatalog().subscribe(console.log);
-  }
 }
